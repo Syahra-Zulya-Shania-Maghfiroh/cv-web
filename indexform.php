f43f2595fd89d509cf34ea2c88db79c2512b161b
syahra <73158627+Syahra-Zulya-Shania-Maghfiroh@users.noreply.github.com>
2021-10-14 18:53:13 +0700

    :D

---
+++ indexform.php
@@ -0,0 +1,27 @@
+<form method="post" action="tampilform.php">
+
+        <label>Masukkan Nama</label><br/>
+        <input type="text" name="nama"><br/>
+        <label>Masukkan Jenis Kelamin</label><br/>
+        <input type="radio" name="gender" value="L">Laki-Laki<br/>
+        <input type="radio" name="gender" value="P">Perempuan<br/>
+        <label>Masukkan Agama</label><br/>
+        <input type="text" name="agama"><br/>
+        <label>Masukkan Alamat</label><br/>
+        <input type="text" name="nama"><br/>
+        <label>Masukkan Tempat Lahir</label><br/> 
+        <input type="text" name="nama"><br/>
+        <label>Masukkan Tanggal Lahir</label><br/> 
+        <input type="date" name="tanggal lahir"><br/> 
+        <label>Masukkan Hobi</label><br/> 
+        <input type="text" name="nama"><br/>
+        <label>Masukkan Cita-cita</label><br/>
+        <input type="text" name="nama"><br/>   
+        <label>Masukkan Usia</label><br/> 
+        <input type="number" name="usia"><br/>
+        <label>Masukkan Asal Sekolah</label><br/>
+        <input type="text" name="asal sekolah"><br/>
+
+        <input type="submit" value="oke">
+
+</form>
